42f291b3c3e6147ec494e77401d5441759b92f1d
Ninjice <31352846+ninjice@users.noreply.github.com>
2017-10-30 11:57:48 -0700

    files

---
+++ CSYes.java
@@ -0,0 +1,19 @@
+// *******************************************************************
+// File Name: Print CS
+// Purpose: Printing a message about computer science
+// *******************************************************************
+ 
+public class CSYes
+{
+   // -------------------------------------------------
+   //  The following main method prints an exciting
+   //  message about computer science
+   // -------------------------------------------------
+   public static void main(String args[]){
+       System.out.println("Computer Science !!!");
+       int length = "Compute Science !!!".length();
+       System.out.println("====================");
+       System.out.println("");
+    }
+}
+ 
